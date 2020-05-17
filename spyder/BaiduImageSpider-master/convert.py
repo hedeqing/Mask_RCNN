87212e86f0be95da12a557deb24f1dba0a357e3c
hedeqing <240484406@qq.com>
2020-05-17 11:10:07 +0800

    add class

--- spyder/BaiduImageSpider-master/convert.py
+++ spyder/BaiduImageSpider-master/convert.py
@@ -1,7 +1,7 @@
 import os
-path = 'G:\datasets\pic'  # path为json文件存放的路径
+path = r'C:\Users\hedeqing\Desktop\Homework\datasets\animals\BaiduImageSpider-master\all\json'  # path为json文件存放的路径
 json_file = os.listdir(path)
 os.system("activate labelme") # 博主labelme所在的环境名就叫labelme，读者应修改成activate [自己labelme所在的环境名]
 for file in json_file:
-    os.system("python D:\Anaconda3\envs\labelme\Scripts/labelme_json_to_dataset.exe %s" % (path + '/' + file))
+    os.system("python D:\Anaconda\envs\labelme\Scripts/labelme_json_to_dataset.exe %s" % (path + '/' + file))
 
--- spyder/BaiduImageSpider-master/deal.py
+++ spyder/BaiduImageSpider-master/deal.py
@@ -3,16 +3,16 @@ import shutil
 filename = "datasets"  # 存放json转化得到的文件夹名称
 fileList = os.listdir(filename)
 for i in range(len(fileList)):
-    # img_source = "datasets/" + fileList[i] + "/img.png"
+    img_source = "datasets/" + fileList[i] + "/img.png"
     mask_source = "datasets/" + fileList[i] + "/label.png"
-    # yaml_source = "datasets/" + fileList[i] + "/info.yaml"
+    yaml_source = "datasets/" + fileList[i] + "/info.yaml"
     print(mask_source)
     # img_target = "pic/img_{}.png".format(i)
     mask_target = "mask/"+fileList[i]+".png"
     mask_target = mask_target.replace('_json','')
     print(mask_target)
-    # yaml_target = "yaml/info_{}.yaml".format(i)
+    yaml_target = "yaml/info_{}.yaml".format(i)
 
     # shutil.copy(img_source, img_target)
     shutil.copy(mask_source, mask_target)
-    # shutil.copy(yaml_source, yaml_target)
+    shutil.copy(yaml_source, yaml_target)
